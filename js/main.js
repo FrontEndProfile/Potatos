95ed0d9742eb759319663816742746365cb1f45d
Fazal <fazalfazail732@gmail.com>
2022-04-04 12:25:44 +0500

    04

---
+++ js/main.js
@@ -0,0 +1,39 @@
+$('.testi').slick({
+  // dots: true,
+  nav: true,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 1,
+  // slidesToScroll: 1,
+  centerPadding:'465px',
+  centerMode: true,
+  draggable:true,
+  responsive: [
+    // {
+    //   breakpoint: 1024,
+    //   settings: {
+    //     slidesToShow: 3,
+    //     slidesToScroll: 3,
+    //     infinite: true,
+    //     dots: true
+    //   }
+    // },
+    // {
+    //   breakpoint: 600,
+    //   settings: {
+    //     slidesToShow: 2,
+    //     slidesToScroll: 2
+    //   }
+    // },
+    // {
+    //   breakpoint: 480,
+    //   settings: {
+    //     slidesToShow: 1,
+    //     slidesToScroll: 1
+    //   }
+    // }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
